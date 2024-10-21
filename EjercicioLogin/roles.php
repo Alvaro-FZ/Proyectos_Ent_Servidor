5e7ad87300c39f8ea5118d15846cbf919ab1816a
Alvaro-FZ <151030461+Alvaro-FZ@users.noreply.github.com>
2024-10-22 00:59:10 +0200

    Add files via upload

---
+++ EjercicioLogin/roles.php
@@ -0,0 +1,10 @@
+<?php
+    // Simulacion de una BBDD con usuarios y sus roles
+    $usuarios = [
+        "admin" => ["password" => "admin123", "rol" => "admin"],
+        "usuario1" => ["password" => "user123", "rol" => "user"],
+        "usuario2" => ["password" => "user456", "rol" => "user"],
+        "manager" => ["password" => "manager123", "rol" => "manager"],
+        "RRHH" => ["password" => "RRHH123", "rol" => "recursos"]
+    ];
+?>
